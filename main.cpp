130c5bdd064d8a5dbd6fe679f18101b6f5527392
Jeremiah Webb <illusion173@hotmail.com>
2023-04-29 13:28:46 -0400

    created a menu for cli

--- main.cpp
+++ main.cpp
@@ -7,13 +7,24 @@
 #include <stdio.h>
 #include <string.h>
 #include <vector>
-enum user_input_code { CFB, CBC, ECB, UNDEFINED };
 
-std::map<int, unsigned char> hexcharacters;
+struct userTest {
+  std::vector<unsigned char> swap;
+  std::vector<unsigned char> user_cipher_text;
+  std::vector<unsigned char> user_plain_vec;
+  std::vector<unsigned char> user_key_vec;
+  std::vector<unsigned char> user_iv_vec;
+  std::string user_key;
+  std::string user_plain;
+  std::string user_iv;
+  int user_choice;
+  int user_choice_encryption_decryption;
+};
 
-void run_user_test(char **);
-user_input_code hashit(char *);
-void initialize_hex_characters();
+void run_user_test_128();
+void run_user_test_192();
+void run_user_test_256();
+void run_user_test_final(AES, userTest);
 
 int main(int argc, char **argv) {
 
@@ -75,7 +86,6 @@ int main(int argc, char **argv) {
   if (first_decrypt_test == initial_plain) {
     std::cout << "128 bit ECB TEST PASS" << std::endl;
   } else {
-
     std::cout << "128 bit ECB TEST FAIL" << std::endl;
   }
 
@@ -99,25 +109,30 @@ int main(int argc, char **argv) {
   // Check if user inputted anything
   // For now we can only take 128 bits
   if (argc > 1) {
-    run_user_test(argv);
+    int user_choice_bytes = 0;
+    printf("Enter a choice:\n");
+    printf("1 for 128 bits\n");
+    printf("2 for 192 bits\n");
+    printf("3 for 256 bits\n");
+    scanf("%d", &user_choice_bytes);
+
+    std::cin.clear();
+    switch (user_choice_bytes) {
+    case 1:
+      run_user_test_128();
+      break;
+    case 2:
+      run_user_test_192();
+      break;
+    case 3:
+      run_user_test_256();
+      break;
+    default:
+      printf("Select a byte length to run user tests.");
+      exit(1);
+    }
   }
   return 0;
-}
-
-// Work around for switches in cpp, since they can't handle strings, we used
-// enums.
-user_input_code hashit(char *selection) {
-
-  if (strcmp("ECB", selection)) {
-    return ECB;
-  }
-  if (strcmp("CFB", selection)) {
-    return CFB;
-  }
-  if (strcmp("CBC", selection)) {
-    return CBC;
-  }
-  return UNDEFINED;
 }
 
 std::vector<unsigned char> convert_to_vec(std::string hex_string) {
@@ -132,9 +147,9 @@ std::vector<unsigned char> convert_to_vec(std::string hex_string) {
   return result;
 }
 
-void run_user_test(char **argv) {
-  // Take first argument as CBC, CFB, or ECB
-  AES user_aes(AESKeyLength::AES_128);
+userTest create_user_test() {
+  // this is pretty grosss, but works.
+  userTest user_test_case;
   std::vector<unsigned char> swap;
   std::vector<unsigned char> user_cipher_text;
   std::vector<unsigned char> user_plain_vec;
@@ -145,6 +160,13 @@ void run_user_test(char **argv) {
   std::string user_iv;
 
   int user_choice = 0;
+  int user_choice_encryption_decryption = 0;
+  printf("Enter a choice:\n");
+  printf("1 for Encrypt\n");
+  printf("2 for Decrypt\n");
+  std::cin >> user_choice_encryption_decryption;
+  std::cin.clear();
+  // Switch statement to deliver menu
   printf("Enter a choice:\n");
   printf("1 for CFB\n");
   printf("2 for ECB\n");
@@ -161,51 +183,124 @@ void run_user_test(char **argv) {
   user_plain_vec = convert_to_vec(user_plain);
   user_key_vec = convert_to_vec(user_key);
 
-  switch (user_choice) {
+  user_test_case.user_plain_vec = user_plain_vec;
+  user_test_case.user_key_vec = user_key_vec;
+  user_test_case.user_choice = user_choice;
+  user_test_case.user_choice_encryption_decryption =
+      user_choice_encryption_decryption;
+
+  return user_test_case;
+}
+
+void run_user_test_final_decrypt(AES user_aes, userTest user_test_case) {
+  switch (user_test_case.user_choice) {
   case 1:
     printf("\n");
     printf("Enter IV: \n");
-    std::cin >> user_iv;
-    user_iv_vec = convert_to_vec(user_iv);
+    std::cin >> user_test_case.user_iv;
+    user_test_case.user_iv_vec = convert_to_vec(user_test_case.user_iv);
+    printf("Performing user test - CFB\n");
+    printf("\nDecrypted Hex: \n");
+    user_test_case.user_cipher_text = user_aes.DecryptCFB(
+        user_test_case.user_plain_vec, user_test_case.user_key_vec,
+        user_test_case.user_iv_vec);
+    user_aes.printHexVector(user_test_case.user_cipher_text);
+    break;
+  case 2:
+    printf("\n");
+    printf("Performing user test - ECB\n");
+    printf("\nDecrypted Hex: \n");
+    user_test_case.user_cipher_text = user_aes.DecryptECB(
+        user_test_case.user_plain_vec, user_test_case.user_key_vec);
+    user_aes.printHexVector(user_test_case.user_cipher_text);
+    break;
+  case 3:
+    printf("\n");
+    printf("Enter IV: \n");
+    std::cin >> user_test_case.user_iv;
+    user_test_case.user_iv_vec = convert_to_vec(user_test_case.user_iv);
+    printf("Performing user test - CBC\n");
+
+    printf("\nDecrypted Hex: \n");
+    user_test_case.user_cipher_text = user_aes.DecryptCBC(
+        user_test_case.user_plain_vec, user_test_case.user_key_vec,
+        user_test_case.user_iv_vec);
+    user_aes.printHexVector(user_test_case.user_cipher_text);
+    break;
+  default:
+    std::cout << "Usage: ./AES T to run user tests." << std::endl;
+    std::cout << "Must enter valid choices." << std::endl;
+    exit(0);
+  }
+}
+
+void run_user_test_final_encrypt(AES user_aes, userTest user_test_case) {
+  switch (user_test_case.user_choice) {
+  case 1:
+    printf("\n");
+    printf("Enter IV: \n");
+    std::cin >> user_test_case.user_iv;
+    user_test_case.user_iv_vec = convert_to_vec(user_test_case.user_iv);
     printf("Performing user test - CFB\n");
     printf("Encrypted Hex: \n");
-    user_cipher_text =
-        user_aes.EncryptCFB(user_plain_vec, user_key_vec, user_iv_vec);
-    user_aes.printHexVector(user_cipher_text);
-    swap = user_cipher_text;
-    printf("\nDecrypted Hex: \n");
-    user_cipher_text = user_aes.DecryptCFB(swap, user_key_vec, user_iv_vec);
-    user_aes.printHexVector(user_cipher_text);
+    user_test_case.user_cipher_text = user_aes.EncryptCFB(
+        user_test_case.user_plain_vec, user_test_case.user_key_vec,
+        user_test_case.user_iv_vec);
+    user_aes.printHexVector(user_test_case.user_cipher_text);
     break;
   case 2:
     printf("\n");
     printf("Performing user test - ECB\n");
     printf("Encrypted Hex: \n");
-    user_cipher_text = user_aes.EncryptECB(user_plain_vec, user_key_vec);
-    user_aes.printHexVector(user_cipher_text);
-    printf("\nDecrypted Hex: \n");
-    swap = user_cipher_text;
-    user_cipher_text = user_aes.DecryptECB(swap, user_key_vec);
-    user_aes.printHexVector(user_cipher_text);
+    user_test_case.user_cipher_text = user_aes.EncryptECB(
+        user_test_case.user_plain_vec, user_test_case.user_key_vec);
+    user_aes.printHexVector(user_test_case.user_cipher_text);
     break;
   case 3:
     printf("\n");
     printf("Enter IV: \n");
-    std::cin >> user_iv;
-    user_iv_vec = convert_to_vec(user_iv);
+    std::cin >> user_test_case.user_iv;
+    user_test_case.user_iv_vec = convert_to_vec(user_test_case.user_iv);
     printf("Performing user test - CBC\n");
     printf("Encrypted Hex: \n");
-    user_cipher_text =
-        user_aes.EncryptCBC(user_plain_vec, user_key_vec, user_iv_vec);
-    user_aes.printHexVector(user_cipher_text);
-
-    printf("\nDecrypted Hex: \n");
-    swap = user_cipher_text;
-    user_cipher_text = user_aes.DecryptCBC(swap, user_key_vec, user_iv_vec);
-    user_aes.printHexVector(user_cipher_text);
+    user_test_case.user_cipher_text = user_aes.EncryptCBC(
+        user_test_case.user_plain_vec, user_test_case.user_key_vec,
+        user_test_case.user_iv_vec);
+    user_aes.printHexVector(user_test_case.user_cipher_text);
     break;
   default:
-    std::cout << "Usage: ./AES mode E/D" << std::endl;
+    std::cout << "Usage: ./AES T to run user tests." << std::endl;
+    std::cout << "Must enter valid choices." << std::endl;
     exit(0);
+  }
+}
+
+void run_user_test_128() {
+  userTest user_test_case = create_user_test();
+  AES user_aes(AESKeyLength::AES_128);
+  if (user_test_case.user_choice_encryption_decryption == 1) {
+    run_user_test_final_encrypt(user_aes, user_test_case);
+  } else if (user_test_case.user_choice_encryption_decryption == 2) {
+    run_user_test_final_decrypt(user_aes, user_test_case);
+  }
+}
+void run_user_test_192() {
+  userTest user_test_case = create_user_test();
+  AES user_aes(AESKeyLength::AES_192);
+  if (user_test_case.user_choice_encryption_decryption == 1) {
+    run_user_test_final_encrypt(user_aes, user_test_case);
+  } else if (user_test_case.user_choice_encryption_decryption == 2) {
+    run_user_test_final_decrypt(user_aes, user_test_case);
+  }
+}
+
+void run_user_test_256() {
+  userTest user_test_case = create_user_test();
+  AES user_aes(AESKeyLength::AES_256);
+
+  if (user_test_case.user_choice_encryption_decryption == 1) {
+    run_user_test_final_encrypt(user_aes, user_test_case);
+  } else if (user_test_case.user_choice_encryption_decryption == 2) {
+    run_user_test_final_decrypt(user_aes, user_test_case);
   }
 }
